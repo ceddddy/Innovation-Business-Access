a3c859ba1484397a2efe14e3c09039dc84c97e4d
Ceddy <ceddddyadvance@gmail.com>
2022-11-12 21:19:15 +0200

    Edit/Change starting files (Replace index.html & style.css with react files

---
+++ innovation-bussiness-access/src/components/index.js
@@ -0,0 +1,10 @@
+
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import Navbar from './Navbar/Navbar';
+
+export {
+  Newsletter,
+  FooterOverlay,
+  Navbar,
+};
---
+++ innovation-bussiness-access/src/constants/images.js
@@ -0,0 +1,11 @@
+import bg from "../assets/bg.png";
+import G from "../assets/G.png";
+import overlaybg from "../assets/overlaybg.png";
+import quote from "../assets/quote.png";
+
+export default {
+  bg,
+  G,
+  overlaybg,
+  quote,
+};
